4d366a2e098b1397f2e301b1574a09ee7227caa3
David Kemeza <dkemeza@gmail.com>
2016-01-19 21:13:09 -0500

    Prelim layout for deck and card class

---
+++ decker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# initial deck and card classes
+
+class deck:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def add_card(self, card_id):
+        pass
+
+
+class card:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def fetch_card(self):
+        pass
