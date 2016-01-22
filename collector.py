92d1c19717d4cd2be20b1aff27f6e4c571b9e618
David Kemeza <dkemeza@gmail.com>
2016-01-22 15:19:36 -0500

    Updated all_cards, and intially converted collector to python3, necessary to fix python2 unicode issues

--- collector.py
+++ collector.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # API testing for netrunnerdb.com
 # api info can be obtiained here http://netrunnerdb.com/en/apidoc
@@ -12,53 +12,53 @@ allcards_base_url = "http://netrunnerdb.com/api/cards/"
 
 url = "http://netrunnerdb.com/api/decklist/30766"
 
-urldata = urllib.urlopen(url)
+#####urldata = urllib.urlopen(url)
 
-raw_deckdata = urldata.read()
+#####raw_deckdata = urldata.read()
 
-deckdata_json = json.loads(str(raw_deckdata))
+#####deckdata_json = json.loads(str(raw_deckdata))
 
 #print deckdata_json
 
 def get_card_name(card_id):
     url = "http://netrunnerdb.com/api/card/" + str(card_id)
-    urldata = urllib.urlopen(url)
-    raw_carddata = urldata.read()
+    #####urldata = urllib.urlopen(url)
+    #####raw_carddata = urldata.read()
     card_json = json.loads(str(raw_carddata))
     #print "card_json", card_json
     return card_json[0]['title']
 
 def get_card_info(card_id):
     url = "http://netrunnerdb.com/api/card/" + str(card_id)
-    urldata = urllib.urlopen(url)
-    raw_carddata = urldata.read()
+    #####urldata = urllib.urlopen(url)
+    #####raw_carddata = urldata.read()
     card_json = json.loads(str(raw_carddata))
     #print "card_json", card_json
     #return card_json[0]['title']
     #print card_json[0]
-    print " Code:" + card_json[0]['code']
-    print "Title:" + card_json[0]['title']
-    print " Type:" + card_json[0]['type']
+    #####print " Code:" + card_json[0]['code']
+    #####print "Title:" + card_json[0]['title']
+    #####print " Type:" + card_json[0]['type']
     #print " Cost:" + str(card_json[0]['cost'])
-    print " Text:" + card_json[0]['text']
+    #####print " Text:" + card_json[0]['text']
 
 def parse_all_cards():
     with open('all_cards.json') as data_file:
         all_cards = json.load(data_file)
 
     for card in all_cards:
-        print card
+        print(card)
         all_keys = []
         #print "Title:" + card['title'] + "Keys:" + str(card.keys())
         for key, value in card.items():
             #print "key:", key.encode('utf-8'), "value:", value.encode('utf-8')
             try:
-                print "key:", str(key), "value:", str(value) #fails on unicode error
+                print("key:", str(key), "value:", str(value)) #fails on unicode error
             except Exception as e:
-                print e
+                print(e)
             if key not in all_keys:
                 all_keys.append(key)
-        print all_keys
+        print(all_keys)
 
 
 #print deckdata_json['cards']
